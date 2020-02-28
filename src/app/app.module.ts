3e70cf935b51f0ddbe3fc450545260f59e6b9589
anvesh <anveshchamakuri20@gmail.com>
2020-02-28 11:59:41 +0530

    these are text project files

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { AboutComponent } from './about/about.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import {RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
